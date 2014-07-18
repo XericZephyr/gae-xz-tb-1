afaa8c1fc3f10c222ea7f523f6d9428184971352
Zheng Xu <xuzheng1111@gmail.com>
2014-07-18 19:39:08 +0800

    First commit of gae-xz-tb-1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, make_response, request
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+import os, urllib2
 
 @app.route('/')
 def hello():
@@ -13,13 +14,16 @@ def hello():
     return 'Hello World!'
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+@app.route('/listenv')
+def listenv():
+    envstr = '\r\n'.join(["%s=%s" % (k, v) for k,v in os.environ.iteritems()])
+    return make_response(envstr, 200, {"Content-Type": "text/plain;charset=utf-8"})
+    
+@app.route('/test-ip')
+def test_ip():
+    t = int(request.args.get('t', 10))
+    ip_list = {}
+    for ip in range(t):
+        ip = urllib2.urlopen("http://www.telize.com/ip").read().strip()
+        ip_list[ip] = ip_list.get(ip, 0) + 1
+    return make_response(str(ip_list), 200, {"Content-Type": "text/plain;charset=utf-8"})
